7738d9e99067f5f4612df420635f8e98a4a6b171
Hack Reactor Student <student@hackreactor.com>
2017-10-13 15:44:34 -0700

    Finished doubly linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -12,15 +12,45 @@ var LinkedList = function() {
     } else if (this.tail === this.head) {
       this.tail = current;
       this.head.next = this.tail;
+      this.tail.previous = this.head;
     } else {
       this.tail.next = current;
+      current.previous = this.tail;
       this.tail = current;
+    }
+  };
+
+  list.addToHead = function(value) {
+    var current = new Node(value);
+    if (this.head === null) {
+      this.head = current;
+      this.tail = current;
+    } else if (this.tail === this.head) {
+      this.head = current;
+      this.head.next = this.tail;
+      this.tail.previous = this.head;
+    } else {
+      this.head.previous = current;
+      current.next = this.head;
+      this.head = current;
     }
   };
 
   list.removeHead = function() {
     var temp = this.head.value;
+    if (this.head !== this.tail) {
+      this.head.next.previous = null;
+    }
     this.head = this.head.next;
+    return temp;
+  };
+
+  list.removeTail = function() {
+    var temp = this.tail.value;
+    if (this.head !== this.tail) {
+      this.tail.previous.next = null;
+    }
+    this.tail = this.tail.previous;
     return temp;
   };
 
@@ -47,7 +77,8 @@ var LinkedList = function() {
 var Node = function(value) {
   var node = {
     'value': value,
-    'next': null
+    'next': null,
+    'previous': null
   };
 
   // node.value = value;
