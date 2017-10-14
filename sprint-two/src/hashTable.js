2b46567230d3601bb80451c4683b9f7f54526e78
Hack Reactor Student <student@hackreactor.com>
2017-10-14 10:28:35 -0700

    Implement rehashing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,6 +3,7 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.tupleCount = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
@@ -12,14 +13,19 @@ HashTable.prototype.insert = function(k, v) {
   } else {
     this._storage[index].push([k, v]);
   }
+
+  this.tupleCount++;
+  this.reHash();
 };
 
 HashTable.prototype.retrieve = function(k) {
   var temp = undefined;
   var index = getIndexBelowMaxForKey(k, this._limit);
-  for (var i = 0; i < this._storage[index].length; i++) {
-    if (this._storage[index][i][0] === k) {
-      temp = this._storage[index][i][1];
+  if (this._storage[index] !== undefined) {
+    for (var i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i][0] === k) {
+        temp = this._storage[index][i][1];
+      }
     }
   }
   return temp;
@@ -32,8 +38,68 @@ HashTable.prototype.remove = function(k) {
       this._storage[index].splice(i, 1);
     }
   }
+
+  this.tupleCount--;
+  this.smallReHash();
 };
 
+HashTable.prototype.smallReHash = function() {
+  var arr = [];
+  if ((this.tupleCount / this._limit) < 0.25) {
+    for (var i = 0; i < this._limit; i++) {
+      if (typeof this._storage[i] !== 'undefined') {
+        for (var j = 0; j < this._storage[i].length; j++) {
+          arr.push(this._storage[i][j]);
+        }
+      }
+    }
+
+    this._limit = Math.floor(this._limit / 2);
+    this.tupleCount = 0;
+    this._storage = LimitedArray(this._limit);
+    for (var k = 0; k < arr.length; k++) {
+      this.insert(arr[k][0], arr[k][1]);
+    }
+  }
+};
+
+HashTable.prototype.reHash = function() {
+  var arr = [];
+
+  if ((this.tupleCount / this._limit) > 0.75) {
+    for (var i = 0; i < this._limit; i++) {
+      if (typeof this._storage[i] !== 'undefined') {
+        for (var j = 0; j < this._storage[i].length; j++) {
+          arr.push(this._storage[i][j]);
+        }
+      }
+    }
+
+    this._limit *= 2;
+    this.tupleCount = 0;
+    this._storage = LimitedArray(this._limit);
+    for (var k = 0; k < arr.length; k++) {
+      this.insert(arr[k][0], arr[k][1]);
+    }
+  }
+};
+
+
+// HashTable.prototype.reHash = function() {
+//   // Count amount of tuples in hashTable
+//   // Compare # of tuples to length of array
+//   // if tuple # > 75% of array.length
+//   //   double the length of array // resizing(2)
+//   // if tuple # < 25% of array.length
+//   //   half the length of array // resizing (.5)
+//   //
+//   // resizing function(value)
+//   // Grab all tuples
+//   // var arr = [all tuples]
+//   // this._limit *= value;
+//   // this._storage = LimitedArray(this._limit);
+//   // this._insert(everything in arr)
+// };
 
 
 /*
@@ -42,5 +108,4 @@ HashTable.prototype.remove = function(k) {
     retrieve = linear/constant
     remove = linear/constant
  */
-
 
